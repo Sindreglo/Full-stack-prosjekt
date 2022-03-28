870401554dd5dc037670e30ab9188a0884edde81
Sindreglo <sindre.glomnes@gmail.com>
2022-03-28 09:27:54 +0200

    init

---
+++ backend/src/test/java/oso/ntnu/idatt2105/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package oso.ntnu.idatt2105.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
