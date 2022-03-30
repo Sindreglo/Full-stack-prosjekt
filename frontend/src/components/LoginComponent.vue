d87d8196e50066b438de928a846655026f688579
Oyvibjon <81357022+Oyvibjon@users.noreply.github.com>
2022-03-30 20:50:00 +0200

    it ain't much but it's honest work

--- frontend/src/components/LoginComponent.vue
+++ frontend/src/components/LoginComponent.vue
@@ -4,14 +4,13 @@
     <form @submit.prevent="login">
       <div>
         <label>Username(email)</label>
-        <input v-model="username" placeholder="Username(email)" type="email" />
+        <input v-model="username" placeholder="Username" type="email" />
         <div v-show="submitted && !username" class="invalid-feedback">
           Username is required
         </div>
         <br />
         <br />
-      </div>
-      <div>
+
         <label>Password</label>
         <input v-model="password" placeholder="Password" type="password" />
         <div v-show="submitted && !password" class="invalid-feedback">
@@ -26,22 +25,23 @@
 </template>
 
 <script>
+import User from "@/model/user";
 export default {
+  name: "LoginComponent",
   data: () => {
     return {
-      username: "",
-      password: "",
+      user: new User("test@test.com", "test"),
       submitted: false,
     };
   },
   computed: {
     /**loggedIn() {
-      return this.$store.state.auth.status.loggedIn();
+      return this.$store.state.auth.status.loggedIn;
     },*/
   },
   created() {
     if (this.loggedIn) {
-      this.$router.push("/subject_page");
+      this.$router.push("/subjects_page");
     }
   },
   methods: {
@@ -49,8 +49,9 @@ export default {
       this.submitted = true;
       const { username, password } = this;
       const { dispatch } = this.$store;
-      if (username && password) {
+      if (this.user.username && this.user.password) {
         dispatch("auth/login", { username, password });
+        console.log("HIT MEN IKKE LENGER");
       }
     },
   },
