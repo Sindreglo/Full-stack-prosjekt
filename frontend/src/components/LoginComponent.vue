136d26f4e26acba1280597f9446abb0e002909fc
Oyvibjon <81357022+Oyvibjon@users.noreply.github.com>
2022-03-30 11:09:07 +0200

    Auth does not work

--- frontend/src/components/LoginComponent.vue
+++ frontend/src/components/LoginComponent.vue
@@ -1,57 +1,56 @@
 <template>
-  <label>Login here</label><br /><br />
-  <div id="loginContainer">
-    <form @submit.prevent="handleLogin">
-      <div id="username">
-        <label>Username(email):</label>
-        <input type="email" name="username" v-model="user.username" />
-      </div>
-      <div id="password">
-        <label>Password:</label>
-        <input type="password" name="password" v-model="user.password" />
+  <div>
+    <h1>LOGIN</h1>
+    <form @submit.prevent="login">
+      <div>
+        <label>Username(email)</label>
+        <input v-model="username" placeholder="Username(email)" type="email" />
+        <div v-show="submitted && !username" class="invalid-feedback">
+          Username is required
+        </div>
+        <br />
         <br />
       </div>
-      <div id="LoginButton">
-        <button type="submit">Sign in</button>
+      <div>
+        <label>Password</label>
+        <input v-model="password" placeholder="Password" type="password" />
+        <div v-show="submitted && !password" class="invalid-feedback">
+          Password is required
+        </div>
+        <br />
+        <br />
       </div>
+      <button type="submit">Login</button>
     </form>
   </div>
 </template>
 
 <script>
-import User from "@/models/user";
 export default {
-  name: "LoginView",
-  data() {
+  data: () => {
     return {
-      user: new User("", ""),
-      error: false,
-      message: "",
+      username: "",
+      password: "",
+      submitted: false,
     };
   },
+  computed: {
+    loggingIn() {
+      return this.$store.state.authentication.status.loggingIn;
+    },
+  },
+  created() {
+    //reset login status
+    this.$store.dispatch("authentication/logout");
+  },
   methods: {
-    handleLogin() {
-      this.loading = true;
-      this.$validator.validateAll().then((isValid) => {
-        if (!isValid) {
-          this.loading = false;
-          return;
-        }
-        if (this.user.username && this.user.password) {
-          this.$store.dispatch("auth/login", this.user).then(
-            () => {
-              this.$router.push("/subject_page");
-            },
-            (error) => {
-              this.loading = false;
-              this.message =
-                (error.response && error.response.data) ||
-                error.message ||
-                error.toString();
-            }
-          );
-        }
-      });
+    login() {
+      this.submitted = true;
+      const { username, password } = this;
+      const { dispatch } = this.$store;
+      if (username && password) {
+        dispatch("authentication/login", { username, password });
+      }
     },
   },
 };
