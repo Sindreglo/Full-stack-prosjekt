003535d0dc14ba96004d039801802249947ad0f4
pistolav <78804567+pistolav@users.noreply.github.com>
2022-04-01 19:25:21 +0200

    Minor changes controller, maby fix

--- backend/src/main/java/oso/ntnu/idatt2015/backend/controller/Course/CourseController.java
+++ backend/src/main/java/oso/ntnu/idatt2015/backend/controller/Course/CourseController.java
@@ -1,6 +1,7 @@
 package oso.ntnu.idatt2015.backend.controller.Course;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,11 +32,11 @@ public class CourseController {
         this.userRoleCourseService = userRoleCourseService;
     }
 
-    @CrossOrigin(origins = "http://localhost:8080/auth")
     //@CrossOrigin
     @GetMapping
-    public CourseResponse getCoursesByUser(HttpServletRequest request){
+    public ResponseEntity<?> getCoursesByUser(HttpServletRequest request){
         String username = jwtUtil.extractUsername(request.getHeader("Authorization").substring(7));
-        return userRoleCourseService.findByUsername(username).header('Access-Control-Allow-Origin','*');;
+        return ResponseEntity.ok(userRoleCourseService.findByUsername(username));
+        //return userRoleCourseService.findByUsername(username).header('Access-Control-Allow-Origin','*');;
     }
 }
